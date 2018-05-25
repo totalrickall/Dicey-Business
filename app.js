f2dbd4f999555b819b18ba489b584ce9fe9c5699
totalrickall <crimsonperry@gmail.com>
2018-05-25 14:28:53 -0500

    more changes

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ button.addEventListener('click', () => {
     contain.style.float = 'left'
     let die = new Die();
     contain.innerText = die.roll();
+    console.log(die);
 
 });
 
 class Die {
     constructor(value) {
-        this.value = randomVal(6,1);
+        this.value = randomVal(7,1);
     };
     roll() {
         return (this.value);
@@ -28,4 +29,9 @@ class Die {
 function randomVal(max, min) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
-//console.log(randomVal(6,1));
+//console.log(randomVal(7,1));
+
+// Create a sumDice function and a button that triggers it. It should add up the current face value of all the dice on the page and display an alert with the sum.
+// Add a feature where clicking on a die on the page causes just that one die to roll, updating its face value
+// Add a feature where double clicking on a die on the page causes that die to be removed from the page
+// Be careful to take appropriate measures so that the sumDice function still reports the correct sum after you delete a die. You need to not only remove the on-screen representation of the Die on the screen, but also remove the Die object from the array as well.
